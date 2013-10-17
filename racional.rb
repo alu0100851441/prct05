32a6c001455a113f0101bcec587aa4ed6dac9b4d
Victor Gonzalez <alu0100851441@ull.edu.es>
2013-10-17 12:41:35 +0100

    first version

--- racional.rb
+++ racional.rb
@@ -3,4 +3,18 @@
 require "./gcd.rb"
 
 class Fraccion
+
+   def initialize(num, den)
+      mcd = gcd(num, den)
+      @n = num / mcd
+      @d = den / mcd
+   end
+
+   def to_s
+      puts @n + '/' + @d
+   end
+
+   def suma(f)
+      puts f.@n
+   end
 end
